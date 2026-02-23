723e3b2075062001f468a41d666dde7d8c662b07
ty-runner <ty.runner02@gmail.com>
2026-02-23 15:16:55 -0800

    main file clean up

--- i2c_driver/i2c_driver.c
+++ i2c_driver/i2c_driver.c
@@ -31,30 +31,6 @@ struct bme280_sensor_packet{ //packet structure for transmission
     uint16_t crc;
 } __attribute__((packed));
 
-/* BME280 calibration data */
-/*struct bme280_calib_data {
-    uint16_t dig_T1;
-    int16_t  dig_T2;
-    int16_t  dig_T3;
-
-    uint16_t dig_P1;
-    int16_t  dig_P2;
-    int16_t  dig_P3;
-    int16_t  dig_P4;
-    int16_t  dig_P5;
-    int16_t  dig_P6;
-    int16_t  dig_P7;
-    int16_t  dig_P8;
-    int16_t  dig_P9;
-
-    uint8_t  dig_H1;
-    int16_t  dig_H2;
-    uint8_t  dig_H3;
-    int16_t  dig_H4;
-    int16_t  dig_H5;
-    int8_t   dig_H6;
-};*/
-
 static struct i2c_device_id my_ids[] = {
     {"tyrunner_bme280", (long unsigned int) &a},
     {},
@@ -74,9 +50,6 @@ static uint16_t read_u16_data(struct i2c_client *client, int reg){
 
     return (uint16_t)((higher << 8) | lower);
 }
-
-//static struct bme280_calib_data calib;
-//static int32_t t_fine;
 
 static void read_calibration_data(struct i2c_client *client){
     calib.dig_T1 = read_u16_data(client, 0x88);
@@ -118,58 +91,6 @@ static int read_bit_data(uint8_t* buffer, struct i2c_client *client, int offset,
     }
     return 0;
 }
-/* Temperature compensation */
-/*static int32_t compensate_temp(int32_t adc_T)
-{
-    int32_t var1, var2;
-    var1 = ((((adc_T >> 3) - ((int32_t)calib.dig_T1 << 1))) *
-            ((int32_t)calib.dig_T2)) >> 11;
-    var2 = (((((adc_T >> 4) - ((int32_t)calib.dig_T1)) *
-              ((adc_T >> 4) - ((int32_t)calib.dig_T1))) >> 12) *
-            ((int32_t)calib.dig_T3)) >> 14;
-    t_fine = var1 + var2;
-    return (t_fine * 5 + 128) >> 8; // 0.01°C
-}*/
-
-/* Pressure compensation */
-/*static uint32_t compensate_pressure(int32_t adc_P)
-{
-    int64_t var1, var2, p;
-    var1 = (int64_t)t_fine - 128000;
-    var2 = var1 * var1 * (int64_t)calib.dig_P6;
-    var2 += ((var1 * (int64_t)calib.dig_P5) << 17);
-    var2 += ((int64_t)calib.dig_P4 << 35);
-    var1 = ((var1 * var1 * (int64_t)calib.dig_P3) >> 8) +
-           ((var1 * (int64_t)calib.dig_P2) << 12);
-    var1 = (((((int64_t)1) << 47) + var1)) * calib.dig_P1 >> 33;
-
-    if (var1 == 0)
-        return 0;
-
-    p = 1048576 - adc_P;
-    p = (((p << 31) - var2) * 3125) / var1;
-    var1 = (calib.dig_P9 * (p >> 13) * (p >> 13)) >> 25;
-    var2 = (calib.dig_P8 * p) >> 19;
-    p = ((p + var1 + var2) >> 8) + ((int64_t)calib.dig_P7 << 4);
-    return (uint32_t)p; // Pa * 256
-}*/
-
-/* Humidity compensation */
-/*static uint32_t compensate_humidity(int32_t adc_H)
-{
-    int32_t v_x1;
-    v_x1 = t_fine - 76800;
-    v_x1 = (((((adc_H << 14) - ((int32_t)calib.dig_H4 << 20) -
-               ((int32_t)calib.dig_H5 * v_x1)) + 16384) >> 15) *
-            (((((((v_x1 * (int32_t)calib.dig_H6) >> 10) *
-                 (((v_x1 * (int32_t)calib.dig_H3) >> 11) + 32768)) >> 10) +
-                 2097152) * (int32_t)calib.dig_H2 + 8192) >> 14));
-    v_x1 -= (((((v_x1 >> 15) * (v_x1 >> 15)) >> 7) * calib.dig_H1) >> 4);
-    if (v_x1 < 0) v_x1 = 0;
-    if (v_x1 > 419430400) v_x1 = 419430400;
-    return (uint32_t)(v_x1 >> 12); // %RH * 1024
-}*/
-
 
 //Grouped data read
 static void bme280_read_all(struct i2c_client *client, int32_t* temp_c, uint32_t* press_pa, uint32_t* humid_rh){
